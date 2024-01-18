e99c4479ebadd06e9223c72c6091f0153e3f0e69
Jay Korat <koratjay03@gmail.com>
2024-01-18 14:24:10 +0530

    110th

---
+++ components/content/courses/fetcher.tsx
@@ -0,0 +1,20 @@
+import courses from "./index.json";
+
+interface Course {
+  id: string;
+  [key: string]: any;
+}
+
+interface CourseMap {
+  [id: string]: Course & { index: number };
+}
+
+export const getAllCourse = (): { data: Course[]; courseMap: CourseMap } => {
+  return {
+    data: courses,
+    courseMap: courses.reduce((a: CourseMap, c: Course, i: number) => {
+      a[c.id] = { ...c, index: i }
+      return a
+    }, {} as CourseMap)
+  }
+}
