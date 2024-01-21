cf9a0ff26e2f6815332193ecd337832c267a33db
Jay Korat <koratjay03@gmail.com>
2024-01-22 02:39:29 +0530

    160th

--- pages/marketplace/index.tsx
+++ pages/marketplace/index.tsx
@@ -37,6 +37,7 @@ export default function Marketplace({courses}:any) {
         />
         <EthRates
           eth={eth.data}
+          ethPerItem={eth.perItem}
         />
         
       </div>
