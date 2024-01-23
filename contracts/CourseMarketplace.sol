36c770b94d3ccfc1617f5c9aa0a6cfe1c60f671d
Jay Korat <koratjay03@gmail.com>
2024-01-24 01:33:24 +0530

    180th

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -8,6 +8,11 @@ contract CourseMarketplace {
         Deactivated
     }
 
+    mapping(bytes32 => Course) private ownedCourses;
+
+    mapping (uint => bytes32) private ownedCourseHash;
+
+    uint private totalOwnedCourses;
 
     struct Course {
         uint id;
@@ -15,5 +20,19 @@ contract CourseMarketplace {
         bytes32 proof;
         address owner;
         State state;
+    }
+
+    function purchaseCourse (bytes16 courseId, bytes32 proof)external payable returns(bytes32){
+        bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+        uint id = totalOwnedCourses++;
+        ownedCourseHash[id] = courseHash;
+        ownedCourses[courseHash] = Course({
+        id: id,
+        price: msg.value,
+        proof: proof,
+        owner: msg.sender,
+        state: State.Purchased
+        });
+         
     }
 }
