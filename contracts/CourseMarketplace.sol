8e307e6f857b20e9619c38e610faa0f8f0f8ea96
Jay Korat <koratjay03@gmail.com>
2024-01-24 10:18:08 +0530

    182nd

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -2,37 +2,79 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract CourseMarketplace {
-    enum State {
-        Purchase,
-        Activated,
-        Deactivated
+  enum State {
+    Purchased,
+    Activated,
+    Deactivated
+  }
+  struct Course {
+    uint id; // 32
+    uint price; // 32
+    bytes32 proof; // 32
+    address owner; // 20
+    State state; // 1
+  }
+  // mapping of courseHash to Course data
+  mapping(bytes32 => Course) private ownedCourses;
+  // mapping of courseID to courseHash
+  mapping(uint => bytes32) private ownedCourseHash;
+  // number of all courses + id of the course
+  uint private totalOwnedCourses;
+
+  /// Course has already a Owner!
+  error CourseHasOwner();
+
+  function purchaseCourse(
+    bytes16 courseId, // 0x00000000000000000000000000003130
+    bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
+  )
+    external
+    payable
+  {
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+
+    if (hasCourseOwnership(courseHash)) {
+      revert CourseHasOwner();
     }
 
-    mapping(bytes32 => Course) private ownedCourses;
+    uint id = totalOwnedCourses++;
 
-    mapping (uint => bytes32) private ownedCourseHash;
+    ownedCourseHash[id] = courseHash;
+    ownedCourses[courseHash] = Course({
+      id: id,
+      price: msg.value,
+      proof: proof,
+      owner: msg.sender,
+      state: State.Purchased
+    });
+  }
+  function getCourseCount()
+    external
+    view
+    returns (uint)
+  {
+    return totalOwnedCourses;
+  }
+  function getCourseHashAtIndex(uint index)
+    external
+    view
+    returns (bytes32)
+  {
+    return ownedCourseHash[index];
+  }
+  function getCourseByHash(bytes32 courseHash)
+    external
+    view
+    returns (Course memory)
+  {
+    return ownedCourses[courseHash];
+  }
 
-    uint private totalOwnedCourses;
-
-    struct Course {
-        uint id;
-        uint price;
-        bytes32 proof;
-        address owner;
-        State state;
-    }
-
-    function purchaseCourse (bytes16 courseId, bytes32 proof)external payable returns(bytes32){
-        bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
-        uint id = totalOwnedCourses++;
-        ownedCourseHash[id] = courseHash;
-        ownedCourses[courseHash] = Course({
-        id: id,
-        price: msg.value,
-        proof: proof,
-        owner: msg.sender,
-        state: State.Purchased
-        });
-         
-    }
+  function hasCourseOwnership(bytes32 courseHash)
+    private
+    view
+    returns (bool)
+  {
+    return ownedCourses[courseHash].owner == msg.sender;
+  }
 }
